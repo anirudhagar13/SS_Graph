91b76d98fabfe7f49e2c34eff0a232c4144779fd
anirudhagar13 <anirudhagarwal13@gmail.com>
2017-03-10 19:28:27 +0530

    Made functions uniform

--- Wordclient.py
+++ Wordclient.py
@@ -4,23 +4,22 @@ from Spider import *
 from Edge import *
 import pdb
 
-def Printpaths(web):
+def Printpaths(word, paths):
 	'''
 	To return web of words around mentioned word
 	'''
-	for key, value in web.items():
-		print ('TO : ',key)
-		for i, path in enumerate(value):
-			print ('PATH',i+1,' :',end='')
-			for edge in path:
-				print (' |',edge, end='')
-			print()
+	print ('TO : ',word)
+	for i, path in enumerate(paths):
+		print ('PATH',i+1,' :',end='')
+		for edge in path:
+			print (' |',edge, end='')
+		print()
 
-def Score(web, word):
+def Score(word, paths):
 	'''
 	To compute score of word in web
 	'''
-	paths = web[word]
+	print('TO : ',word)
 	score = 0
 	for i, path in enumerate(paths):
 		path_score = 1
@@ -31,8 +30,8 @@ def Score(web, word):
 
 if __name__ == '__main__':
 	# pdb.set_trace()
-	word = 'dog'
-	sp = Spider(word)
-	answer = sp.crawl()
-	Printpaths(answer)
-	print (Score(answer,'man'))
+	word = 'man'
+	sp = Spider('dog')
+	web = sp.crawl()
+	Printpaths(word, web[word])
+	print (Score(word, web[word]))
