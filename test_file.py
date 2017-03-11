283c6b918f0e8afb93946d99e8b6411f7c2b1bd0
Rohan <rohanagarwal6@gmail.com>
2017-03-11 14:36:17 +0530

    additions to test_file.py

--- test_file.py
+++ test_file.py
@@ -4,6 +4,7 @@ from random import randint
 import shelve
 from nltk.corpus import wordnet as wn
 from Commons import Unicode
+import collections
 
 def get_words_test():
     definition = 'This is usually in the United States where we find it.'
@@ -38,8 +39,24 @@ def word_data_d2s_test(wd_filename):
             assert find_substring(keys[index], wn.synset(k).definition())
         print '********PASS********'
 
+def sense_processing_test(sd_filename):
+    sense_data = shelve.open(sd_filename)
+    #print len(sense_data.keys())
+    keys = sense_data.keys()
+    max_rng = len(keys)
+    #print keys[:5]
+    print sense_data['christian_holy_day.n.01']['S2W']
+    '''
+    for trials in range(0,10):
+        index = randint(0,max_rng-1)
+        #keys[index] -> string value - sense
+        keys[index] = synset_name
+        assert collections.Counter(sense_data[synset_name]['S2W']) == collections.Counter(wn.synset(synset_name).lemma_names())
+    '''
+
 if __name__ == "__main__":
 
     #get_words_test()
     #print 'united' in 'united_states'
-    word_data_d2s_test('Hash#3_hotfix.shelve')
+    #word_data_d2s_test('Hash#3_hotfix.shelve')
+    sense_processing_test('Shelves/Hash#4.shelve')
